03977d478f4fc162345394af5e3fa4778187409e
admin <admin@DESKTOP-UIOVGPM>
2019-01-26 18:17:58 +0700

    add some bullshit

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameState : MonoBehaviour
-{
+public class GameState : MonoBehaviour {
+
+    private PlayerController[] players;
+    private int turn;
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
+
+
+    private int healthPoint;
+    private string[] moves;
+    private int playerNumber;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.healthPoint = 100;
+        this.moves = new string[5];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (i = 0; i < moves.Length; i++) {
+            calculateMove(moves[i]);
+        }
+    }
+
+    void calculateMove(string move)
+    {
+
     }
 }
